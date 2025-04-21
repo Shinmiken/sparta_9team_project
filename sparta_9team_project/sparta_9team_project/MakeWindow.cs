54b7e4960db51ecf6895f68c7ecd758fe09f552f
Shinmiken <zxcvbnmsib@gmail.com>
2025-04-21 21:12:07 +0900

    int : cmd 창 조절
    창 조절하는 클래스 추가했습니다.

---
+++ sparta_9team_project/sparta_9team_project/MakeWindow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace sparta_9team_project
+{
+    public class ConsoleManager
+    {
+        public static void ConfigureConsoleSize()
+        {
+            Console.SetWindowSize(150, 40);     // 실제 보이는 창
+            Console.SetBufferSize(150, 100);    // 스크롤 가능 크기
+        }
+
+        public static void PrintCentered(string text, int y)
+        {
+            int x = (Console.WindowWidth - text.Length) / 2;
+            Console.SetCursorPosition(x - 10, y);
+            Console.WriteLine(text);
+        }
+
+        public static void PrintAsciiAt(string ascart, int x, int y)
+        {
+            string[] lines = ascart.Split('\n');
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.SetCursorPosition(x, y + i);
+                Console.WriteLine(lines[i]);
+            }
+        }
+    }
+
+}
