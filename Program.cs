64d4993c30478e667ad45fbdb5887778e38f0864
Shinmiken <zxcvbnmsib@gmail.com>
2025-04-21 16:08:14 +0900

    main : 메인입니다
    메인입니다

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sparta_9team_project;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.ReadKey();
+    }
+}
