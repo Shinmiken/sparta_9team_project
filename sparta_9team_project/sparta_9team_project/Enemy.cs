827db3044321880f3a326c028c10e730067cdfe9
ChoiGunWoo1 <gunwoo0813@naver.com>
2025-04-22 19:48:51 +0900

    변수 이름 통일
    변수이름 통일했습니다.

--- sparta_9team_project/sparta_9team_project/Enemy.cs
+++ sparta_9team_project/sparta_9team_project/Enemy.cs
@@ -1,43 +1,80 @@
 ﻿using System;
+using ConsoleManager;
+
 
 namespace sparta_9team_project
 {
-    public static string enemypicture[] = new string[3]; //enemy asciiart를 넣어야함..(아직 안넣음)
+    Random rand = new Random();
 
-
-
-    public enum enemytype
+    public enum enemytype // 적 타입 enum형태로 표현
     {
         chihuahua,   //치와와
         husky,       //허스키
         mortorcycle  //오토바이
-
         // 더 추가가능...
     }
-    public class Enemy
+
+
+    public struct Enemyinfo // 적 정보를 모아놓은 구조체
+    {
+        public int level;
+        public int attack;
+        public int defense;
+        public int hpoint;
+        public int gld;
+        public string atkname;
+        public string enepic; // 적 그림
+        public int mhp; // 최대 hp
+        public enemytype enetyp; // 적 타입
+    }
+
+
+    public class Enemyinfos
+    {
+        public static Enemyinfo enemyinfos[] = new Enemyinfo[3]; // 각 enmyinfo를 미리 배열에 저장
+        // 0, 1, 2에 각각 enemy 정보 저장...
+    }
+
+    public class Enemy : Character
     {
         // character class에서 공격력, 레벨등등의 정보를 상속받음
-        private string attackname { get; set; } // 공격 이름(ex, 물어뜯기...)
-        private enemytype enetyp { get; set; }
+        private string attackname; // 공격 이름(ex, 물어뜯기...)
         private string enemypic;
         private int maxhp;
-
-        Enemy(int level, int attack, int defense, int hpoint, int gld, string atkname, enemytype type)
+        enemytype enetype;
+        Enemy(enemytype type) // 각 멤버변수 type에 맞게 초기화
         {
-            lvl = level;
-            atk = attack;
-            def = defense;
-            hp = hpoint;
-            maxhp = hpoint;
-            gold = gld;
-            attackname = atkname;
-            enetype = type;                     // 적 종류 체크
-            enemypic = enemypicture[(int)type]; // 적 모습 체크
-        }
+            lvl = Enemyinfos.enemyinfos[(int)type].level;
+            atk = Enemyinfos.enemyinfos[(int)type].attack;
+            def = Enemyinfos.enemyinfos[(int)type].defense;
+            hp = Enemyinfos.enemyinfos[(int)type].hpoint;
+            gold = Enemyinfos.enemyinfos[(int)type].gld;
 
+            maxhp = Enemyinfos.enemyinfos[(int)type].mhp;
+            attackname = Enemyinfos.enemyinfos[(int)type].atkname;
+            enetype = Enemyinfos.enemyinfos[(int)type].enetyp;   // 적 종류 체크
+            enemypic = Enemyinfos.enemyinfos[(int)type].enepic;  // 적 모습 체크
+        }
+    }
+    public class Enimies
+    {
+        private Enemy[] enimies;
+        private int enemycount;
+
+        Enimies(int ecount) // enemycount : 적들의 수
+        {
+            enemycount = ecount;
+            enimes = new Enemy[ecount]; // 적들의 수를 받아서 enemy 배열을 생성.
+            for(int i = 0; i < ecount; i++)
+            {
+                int t = rand.Next(0, 3);
+                enimies[i] = new Enemy(t);
+            }
+
+        }
         public displayenemypicture(int x, int y) // 적의 모습을 화면에 띄우는 함수
         {
-            PrintAsciiAt(enemypic, x, y);
+            GameManager.
         }
 
         public displayenemyinfo() // 적의 정보를 화면에 띄우는 함수
