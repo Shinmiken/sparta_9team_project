2af27c216781e4d2d477df34f3909bd9e865b89e
juleswno <reve0107@gmail.com>
2025-04-22 20:08:42 +0900

    showstatus제거_Character.cs

--- sparta_9team_project/sparta_9team_project/Character.cs
+++ sparta_9team_project/sparta_9team_project/Character.cs
@@ -20,48 +20,5 @@ namespace sparta_9team_project
             this.Hp = Hp;
             this.Gold = Gold;
         }
-
-        /*
-        public void ShowStatus()
-        {
-            Console.WriteLine();
-            Console.WriteLine("상태 보기");
-            Console.WriteLine("캐릭터의 정보가 표시됩니다.");
-            Console.WriteLine();
-            Console.WriteLine($"Lv. {Level:00}");
-            Console.WriteLine($"{Name}"); // (Job) 부분 제거했어
-            Console.WriteLine($"공격력 : {Atk}");
-            Console.WriteLine($"방어력 : {Def}");
-            Console.WriteLine($"체 력 : {Hp}");
-            Console.WriteLine($"Gold : {Gold} G");
-            Console.WriteLine();
-            Console.WriteLine("[0. 나가기]");
-            Console.WriteLine();
-            Console.Write("원하시는 행동을 입력해주세요.\n>> ");
-        }
-
-        public void ShowStatusWithExit()
-        {
-            bool isRunning = true;
-            while (isRunning)
-            {
-                ShowStatus();
-                Console.Write("원하시는 행동을 입력해주세요.\n>> ");
-
-                string input = Console.ReadLine();
-
-                switch (input)
-                {
-                    case "0":
-                        Console.WriteLine("상태 보기를 종료합니다.");
-                        isRunning = false;
-                        break;
-                    default:
-                        Console.WriteLine("잘못된 입력입니다.");
-                        break;
-                }
-            }
-        }
-        */
     }
 }
