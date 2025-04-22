bd850cbc0d01f0215dd18d0bd83a73e060d5b5b1
juleswno <reve0107@gmail.com>
2025-04-22 17:07:16 +0900

    Update Character.cs

--- sparta_9team_project/sparta_9team_project/Character.cs
+++ sparta_9team_project/sparta_9team_project/Character.cs
@@ -4,6 +4,24 @@ namespace sparta_9team_project
 {
     public class Character
     {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int Level { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Hp { get; set; }
+        public int Gold { get; set; }
 
+        // [Constructor]
+        public Character(string name, string job, int level, int attack, int defense, int hp, int gold)
+        {
+            Name = name;
+            Job = job;
+            Level = level;
+            Attack = attack;
+            Defense = defense;
+            Hp = hp;
+            Gold = gold;
+        }
     }
 }
