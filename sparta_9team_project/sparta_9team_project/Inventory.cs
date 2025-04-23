1729b2bc0ab679fd826700abf1501b610ee6d68d
jaylynnmell <danmugi118@gmail.com>
2025-04-23 19:34:08 +0900

    feat, update : Item, Player, Inventory클래스 관련 기능 업데이트 및 추가
    01) Item클래스 구현
    02) Inventory클래스 구현중
    03) Player클래스 메소드 업데이트

--- sparta_9team_project/sparta_9team_project/Inventory.cs
+++ sparta_9team_project/sparta_9team_project/Inventory.cs
@@ -8,6 +8,17 @@ namespace sparta_9team_project
 {
     class Inventory
     {
+        Player player = PlayerManager.instance.mainPlayer;
+
         private List<Item> items = new List<Item>();
+        public void AddItem(Item item)
+        {
+            if (items.Contains(item))
+            {
+                //내용 들어갈것
+            }
+            items.Add(item);
+            Console.WriteLine($"{player.Name}의 소지품에 {item.Name}이(가) 인벤토리에 추가되었습니다.");
+        }
     }
 }
--- sparta_9team_project/sparta_9team_project/Item.cs
+++ sparta_9team_project/sparta_9team_project/Item.cs
@@ -1,12 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace sparta_9team_project
+﻿namespace sparta_9team_project
 {
-    class Item
+    public enum ItemType
     {
+        무기,
+        방어구,
+        소모품,
+    }
+
+    // [Item] 클래스 - 부모 클래스 & 아이템 데이터베이스
+    public class Item
+    {
+        // [Fields]
+        public string Name { get; set; }
+        public ItemType Type { get; set; }
+        public int Counts { get; set; } = 1; // 아이템 개수
+        public string Description { get; set; }
+        
+
+        // [Constructor]
+        public Item(string name, ItemType type, int counts, string description)
+        {
+            Name = name;
+            Type = type;
+            Counts = counts;
+            Description = description;
+        }
+
+        // [Methods]
+        public virtual void UseItem(Player player)
+        {
+            if (Counts <= 0)
+            {
+                Counts = 0;
+                Console.WriteLine($"{Name}이(가) 없습니다.");
+            }
+        }
+    }
+
+    public class ItemDataBase
+    {
+        // 무기
+
+        // 방어구
+
+        // 소모품
+        public static Consumable smallHealingPotion = new Consumable(30, "힐링포션(소)", ItemType.소모품, 1, "체력을 30 회복합니다.");
+    }
+
+    // [Item]클래스를 상속받은 자식 클래스들
+    public class Weapon : Item
+    {
+        public int AttackPower { get; set; }
+        public Weapon(int attackPower, string name, ItemType type, int counts, string description ) : base(name, ItemType.무기, counts, description)
+        {
+            AttackPower = attackPower;
+        }
+    }
+
+    public class Armor : Item
+    {
+        public int DefensePower { get; set; }
+        public Armor (int defensePower, string name, ItemType type, int counts, string description) : base(name, ItemType.방어구, counts, description)
+        {
+            DefensePower = defensePower;
+        }
+    }
+
+    public class Consumable : Item
+    {
+        public int EffectAmount { get; set; }
+        public Consumable (int effectAmount, string name, ItemType type, int counts, string description) : base(name, ItemType.소모품, counts, description)
+        {
+            EffectAmount = effectAmount;
+        }
+
+        public override void UseItem(Player player)
+        {
+            base.UseItem(player);
+
+            Counts -= 1; // 사용한 아이템 개수 
+            Console.WriteLine($"{player.Name}이(가) {Name}을(를) 사용했습니다.");
+
+        }
     }
 }
