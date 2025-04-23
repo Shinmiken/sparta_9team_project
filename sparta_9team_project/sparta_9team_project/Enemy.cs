f0347fde9e83729e49c5b15f8d2a5beb858773de
ChoiGunWoo1 <gunwoo0813@naver.com>
2025-04-23 20:07:18 +0900

    GentEnemyInfo함수 추가
    현재 적의 정보를 불러오는 GetEnemyinfo함수를 작성했습니다

--- sparta_9team_project/sparta_9team_project/Enemy.cs
+++ sparta_9team_project/sparta_9team_project/Enemy.cs
@@ -83,18 +83,27 @@ namespace sparta_9team_project
 
         public void displayenemyinfo() // 적의 정보를 화면에 띄우는 함수
         {
-
+           
         }
 
         public bool EnemygetDamage(int enemynum, int damage) // enemynum번호의 몹이 데미지를 받는 동작을 하는 함수
         {
             if (enemynum  <= 0 || enemynum >= enemycount) return false;
             if (enemies[enemynum].Hp == 0) return false;
-            enemies[enemynum].GetDamage(damage);
+            enemies[enemynum-1].GetDamage(damage);
             return true;
             // 만약 잘못된 enemynum이나 이미 죽은 몹이면 false를 반환하여 잘못된 입력임을 알려줌
             // 입력이 제대로 되었다면 체력 깎는 GetDamage함수를 실행시키고 true를 반환하여 성공을 알려줌.
         }
+        
+        public bool GetEnemyInfo(int input, ref Enemy Ei)
+        {
+            if(input <= 0 || input >= enemycount) {  return false; }
+            Ei = enemies[input-1];
+            return true;
+        }
+        // input이 잘못된 입력이면 false를 반환하고 Ei에 enemy 정보를 입력하지 않음
+        // 맞는 입력이라면 true를 반환하고 Ei에 enemy 정보를 입력
     }
 }
 
@@ -106,4 +115,9 @@ namespace sparta_9team_project
 //                                -> 1번 몹이 10 데미지를 받는데, 만약 1번몹이 이미 죽었다면 false를 반환
 //                                   맞는 몹 선택이라면 데미지를 받게하고 true를 반환
 //                                   이 true/false 반환으로 맞는 선택을했는지 알 수 있음 -> 활용 가능
+// 던전에서 1번 적의 현재 정보를 확인하고 싶다면 :
+//                               Enemy enemy1;
+//                               dungeonmob.GetEnemyInfo(1, enemy1);
+//                               1번 몹의 정보가 enemy1에 들어가게 됨 그리고 true 반환
+//                               잘못된 번호의 입력이라면 false 반환
 // 추가 예정...
