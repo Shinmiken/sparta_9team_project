65c82442bd0dc6ee709bd2be7a1cefa21bc8865d
jaylynnmell <danmugi118@gmail.com>
2025-04-23 20:46:55 +0900

    Update : Consumable클래스 UseItem()메소드 업데이트
    01) Consumable클래스 UseItem()메소드 업데이트

--- sparta_9team_project/sparta_9team_project/Item.cs
+++ sparta_9team_project/sparta_9team_project/Item.cs
@@ -7,6 +7,15 @@
         소모품,
     }
 
+    public enum ConsumableEffect
+    {
+        체력회복,
+        공격력증가,
+        방어력증가,
+    }
+
+    /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
     // [Item] 클래스 - 부모 클래스 & 아이템 데이터베이스
     public class Item
     {
@@ -27,25 +36,27 @@
         }
 
         // [Methods]
-        public virtual void UseItem(Player player)
+        public virtual void UseItem(Player player) 
         {
             if (Counts <= 0)
             {
-                Counts = 0;
-                Console.WriteLine($"{Name}이(가) 없습니다.");
+                Console.WriteLine($"{player.Name}의 소지품에 {Name}이 없습니다.");
+                return;
             }
         }
     }
 
-    public class ItemDataBase
+    public static class ItemDataBase
     {
         // 무기
 
         // 방어구
 
         // 소모품
-        public static Consumable smallHealingPotion = new Consumable(30, "힐링포션(소)", ItemType.소모품, 1, "체력을 30 회복합니다.");
+        public static Consumable smallHealingPotion = new Consumable(30, "힐링포션(소)", ItemType.소모품, ConsumableEffect.체력회복, 1, "체력을 30 회복합니다.");
     }
+
+    /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
     // [Item]클래스를 상속받은 자식 클래스들
     public class Weapon : Item
@@ -68,18 +79,39 @@
 
     public class Consumable : Item
     {
+        // [Fields]
         public int EffectAmount { get; set; }
-        public Consumable (int effectAmount, string name, ItemType type, int counts, string description) : base(name, ItemType.소모품, counts, description)
+        public ConsumableEffect EffectType { get; set; }
+
+        // [Constructor]
+        public Consumable (int effectAmount, string name, ItemType type, ConsumableEffect effectType, int counts, string description) : base(name, ItemType.소모품, counts, description)
         {
             EffectAmount = effectAmount;
+            EffectType = effectType;
+
         }
 
+        // [Methods]
+
+        // UseItem() 사용법:
+        // 01) ItemDataBase.smallHealingPotion.UseItem(player);
         public override void UseItem(Player player)
         {
-            base.UseItem(player);
+            base.UseItem(player);                                                       // 아이템이 인벤토리에 없다면
+            
+            switch (EffectType)                                                         // 아이템이 인벤토리에 있다면 && ConsumableEffect 타입 확인
+            {
+                case ConsumableEffect.체력회복:
+                    int needsHeal = player.MaxHp - player.Hp;                           // 현재 체력과 최대 체력의 차이
+                    int actualHealAmount = Math.Min(EffectAmount, needsHeal);           // 실제 회복량
+                    player.Hp += actualHealAmount;                                      // 플레이어 체력 회복
 
-            Counts -= 1; // 사용한 아이템 개수 
-            Console.WriteLine($"{player.Name}이(가) {Name}을(를) 사용했습니다.");
+                    Counts -= 1;                                                        // 사용된 아이템 개수 감소
+                    Console.WriteLine($"{player.Name}이(가) {Name}을(를) 사용했습니다.");
+                    break;
+
+                    //아이템 추가 가능
+            }
 
         }
     }
