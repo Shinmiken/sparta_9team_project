5767c238a8f8e4c426697ca7d2817794ef3c3f36
juleswno <reve0107@gmail.com>
2025-04-27 23:11:35 +0900

    캣닢 관련 수정_DropManager.cs

--- sparta_9team_project/sparta_9team_project/DropManager.cs
+++ sparta_9team_project/sparta_9team_project/DropManager.cs
@@ -9,7 +9,16 @@ namespace sparta_9team_project
         public static bool TryDropCatnip()
         {
             int chance = rng.Next(0, 100);
-            return chance < 5;  // 5% 확률 
+            return chance < 5;
+        }
+
+        public static void TryGiveCatnip() 
+        {
+            if (TryDropCatnip())
+            {
+                InventoryManager.Instance.PlayerInventory.AddItem(ItemDataBase.catnip);
+                Console.WriteLine("어디선가 캣닢 향기가 난다!");
+            }
         }
     }
 }
