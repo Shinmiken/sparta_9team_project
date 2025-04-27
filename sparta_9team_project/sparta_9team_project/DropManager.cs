72068035a39ad31c890a529b07a846ad7b6f42f9
juleswno <reve0107@gmail.com>
2025-04-27 21:19:11 +0900

    캣닢5%확률용_DropManager.cs

--- sparta_9team_project/sparta_9team_project/DropManager.cs
+++ sparta_9team_project/sparta_9team_project/DropManager.cs
@@ -9,7 +9,7 @@ namespace sparta_9team_project
         public static bool TryDropCatnip()
         {
             int chance = rng.Next(0, 100);
-            return chance < 5;  // ✅ 0~4 (총 5개) → 5% 확률 성공
+            return chance < 5;  // 5% 확률 
         }
     }
 }
